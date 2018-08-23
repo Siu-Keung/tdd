2db2a88967001eddfbf4d0bd14c0dbca949d3834
SiuKeung <siu_keung@outlook.com>
2018-08-23 13:49:37 +0800

    dylan: implemented returning "a" given "a"

--- src/main/java/PermutationGenerator.java
+++ src/main/java/PermutationGenerator.java
@@ -1,11 +1,12 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PermutationGenerator {
 
     public static List<String> generatePermutation(String input){
-        throw new NotImplementedException();
+        return Arrays.asList(input);
     }
 
 }
